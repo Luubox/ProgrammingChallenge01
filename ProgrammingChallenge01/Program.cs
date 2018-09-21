069af40099a2719d5771fc5aa2d707273ff1d877
J Nielsen <buhros@hotmail.com>
2018-09-21 09:19:05 +0200

    Initial commit

---
+++ ProgrammingChallenge01/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingChallenge01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NumberChecker();
+            Console.ReadKey();
+        }
+
+        private static void NumberChecker()
+        {
+            Console.WriteLine("Please enter a number:");
+            int inputNumber = Convert.ToInt32(Console.ReadLine());
+            if (inputNumber > 0)
+            {
+                Console.WriteLine($"The number {inputNumber} is greater than 0");
+                NumberChecker();
+            }
+            if (inputNumber < 0)
+            {
+                Console.WriteLine($"The number {inputNumber} is less than 0");
+                NumberChecker();
+            }
+            else
+            {
+                Console.WriteLine($"The number {inputNumber} is equal to 0");
+                NumberChecker();
+            }
+        }
+    }
+}
---
+++ WhichIsGreater/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhichIsGreater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NumberChecker();
+            Console.ReadKey();
+        }
+
+        private static void NumberChecker()
+        {
+            Console.WriteLine("Please enter a number:");
+            int firstInput = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter another number:");
+            int secondInput = Convert.ToInt32(Console.ReadLine());
+
+            //if (firstInput == secondInput)
+            //{
+            //    Console.WriteLine($"The number {firstInput} is the same as {secondInput}");
+            //    NumberChecker();
+            //}
+            if (firstInput > secondInput)
+            {
+                Console.WriteLine($"The number {firstInput} is greater than {secondInput}");
+                NumberChecker();
+            }
+            else /* (firstInput < secondInput)*/
+            {
+                Console.WriteLine($"The number {secondInput} is greater than {firstInput}");
+                NumberChecker();
+            }
+        }
+    }
+}
