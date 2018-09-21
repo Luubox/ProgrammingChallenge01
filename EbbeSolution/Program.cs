302dee5dff41b8c5982d8d7ac08082714456d56f
J Nielsen <buhros@hotmail.com>
2018-09-21 09:56:30 +0200

    Ebbes Løsning

---
+++ EbbeSolution/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EbbeSolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tal;
+            GetNumber();
+            NumberAnalyzer(tal);
+        }
+
+        private static int GetNumber()
+        {
+            Console.Write("Skriv et tal:");
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+        private static string NumberAnalyzer(int tal)
+        {
+            if (tal == 0) return "Tallet er nul";
+            else if (tal > 0) return "Tallet er positivt";
+            else return "Tallet er negativt";
+        }
+    }
+}
